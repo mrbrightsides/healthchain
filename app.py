602774960dfee184807647de26b73e1b1ce2b50f
Akhmad Khudri <khudri@binadarma.ac.id>
2025-09-24 15:46:57 +0700

    Fix syntax error in set_page_config call

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 st.set_page_config(
-    page_title="HealthChain"
-",
+    page_title="HealthChain",
     page_icon="🏥",
     layout="wide"
 )
